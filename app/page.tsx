8770ed10b29f05a97110d321d8b55950ba0d62f6
Kevin March <kevinmarch@cds.com.py>
2026-01-26 17:51:17 -0300

    Inittial Commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <section className="py-24">
+      <div className="container mx-auto px-4">
+        <h1 className="text-4xl font-bold mb-8 text-center">
+          Welcome to Our Next.js App!
+        </h1>
+        <p className="text-lg text-center">
+          This is the homepage of our awesome Next.js application. Explore the
+          features and enjoy your stay!
+        </p>
+      </div>
+    </section>
+  );
+}
